20e72c4e1aa275beb3474697e217444a2bc04048
Miklos Barabas <miklos.barabas@betvictor.com>
2016-09-22 19:07:00 +0200

    Switching to Jetty + ALPN

--- src/main/java/com/example/project/App.java
+++ src/main/java/com/example/project/App.java
@@ -4,11 +4,12 @@ package com.example.project;
  * Created by Miklos barabas on 22/09/2016.
  */
 
-import org.apache.coyote.http2.Http2Protocol;
+import org.eclipse.jetty.servlets.PushCacheFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication // == @Configuration @EnableAutoConfiguration @ComponentScan
@@ -17,20 +18,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(App.class);
         //app.setShowBanner(false);
-        tomcatCustomizer();
         TomcatHTTP2.tomcatCustomizer();
         app.run(args);
 
     }
 
     @Bean
-    public static EmbeddedServletContainerCustomizer tomcatCustomizer() {
-        return (container) -> {
-            if (container instanceof TomcatEmbeddedServletContainerFactory) {
-                ((TomcatEmbeddedServletContainerFactory) container)
-                        .addConnectorCustomizers((connector) -> connector.addUpgradeProtocol(new Http2Protocol()));
-            }
-        };
+    public EmbeddedServletContainerCustomizer jettyHttp2Customizer(ServerProperties serverProperties) {
+        return new JettyHttp2Customizer(serverProperties);
+    }
+
+    @Bean
+    public FilterRegistrationBean pushCacheFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new PushCacheFilter());
+        return registration;
     }
 
 
