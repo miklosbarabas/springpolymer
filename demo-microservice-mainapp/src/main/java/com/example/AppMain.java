8d0027b4d85e0f53e76eadc8e70172db6a1a342a
Miklos Barabas <miklos.barabas@betvictor.com>
2016-10-03 15:19:36 +0200

    Added Spring Cloud components (Zookeeper, Zuul, Hystrix)

--- demo-microservice-mainapp/src/main/java/com/example/AppMain.java
+++ demo-microservice-mainapp/src/main/java/com/example/AppMain.java
@@ -4,20 +4,29 @@ package com.example;
  * Created by Miklos Barabas on 22/09/2016.
  */
 
+import org.apache.curator.test.TestingServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication // == @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class AppMain {
 
+    @Bean
+    TestingServer testingServer() throws Exception {
+        return new TestingServer(2181);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(AppMain.class);
-//        TomcatHTTP2.tomcatCustomizer();
         app.run(args);
-
     }
 
+    /* UNCOMMENT THE CODE ABOVE & JettyHttp2Customizer TO ENABLE HTTP2 */
 //    @Bean
 //    public EmbeddedServletContainerCustomizer jettyHttp2Customizer(ServerProperties serverProperties) {
 //        return new JettyHttp2Customizer(serverProperties);
